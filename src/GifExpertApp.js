f6bd07ddd2a0ac957f9974639be6a8329f8d90e3
Fernando Nicolas <fersanti2896@gmail.com>
2022-01-09 14:09:32 -0600

    Iniciando proyecto de GifExpertApp y agregando primer componente

---
+++ src/GifExpertApp.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const GifExpertApp = () => {
+    return (
+        <>
+            <h2>GifExpert App</h2>
+            <hr />
+        </>
+    );
+}
+
+export default GifExpertApp;
