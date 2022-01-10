15b0ad76fd8f238b64f73b9de2db3bb5aca33007
Fernando Nicolas <fersanti2896@gmail.com>
2022-01-10 00:17:59 -0600

    Se agrega el hook useEffect al componente GifGrid, con esto, solo se hace la peticion a la API una vez y no varias veces al rederizar el componente

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,6 +1,13 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 
 export const GifGrid = ({ category }) => {
+    const [count, setCount] = useState(0);
+
+    /* Ejecuta código de manera condicional - useEffect */
+    useEffect(() => {
+        getGifs();
+    }, [])
+
     const getGifs = async() => {
         const url = 'https://api.giphy.com/v1/gifs/search?q=Shrek&limit=10&api_key=mIKmP6pvCvfLHB6anJ2yXEStStuSFt5W';
         const resp = await fetch(url);
@@ -17,10 +24,11 @@ export const GifGrid = ({ category }) => {
         console.log(gifs);
     }
 
-    getGifs();
     return (
         <>
-            <h3>{ category }</h3>  
+            <h3>{ category }</h3>
+            <h2>{ count }</h2> 
+            <button onClick = {() => setCount(count + 1)}/>
         </>
     )
 }
